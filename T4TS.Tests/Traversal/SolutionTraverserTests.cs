6092bf96d4a553b30c3bdeb0a26035f4ffaea55f
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2014-12-10 23:05:37 +0100

    Add solution traverser tests

---
+++ T4TS.Tests/Traversal/SolutionTraverserTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using T4TS.Example.Models;
+using T4TS.Tests.Models;
+using T4TS.Tests.Utils;
+
+namespace T4TS.Tests.Traversal
+{
+    [TestClass]
+    public class SolutionTraverserTests
+    {
+        [TestMethod]
+        public void ShouldVisitEachNamespace()
+        {
+            var solution = DTETransformer.BuildDteSolution(
+                typeof(LocalModel),
+                typeof(ModelFromDifferentProject)
+            );
+
+            int callCount = 0;
+            var expectedNames = new string[] { "T4TS.Tests.Models", "T4TS.Example.Models" };
+
+            new SolutionTraverser(solution, (ns) =>
+            {
+                Assert.AreEqual(expectedNames[callCount++], ns.Name);
+            });
+
+            Assert.AreEqual(2, callCount);
+        }
+    }
+}
